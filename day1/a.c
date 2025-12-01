6a5210372c6277110fd655e1a5e1a2c4e7c11569
Tom Groot <4732713+Tomgroot@users.noreply.github.com>
2025-12-01 21:11:51 +0100

    day 1 [c]

---
+++ day1/a.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    int current_value = 50;
+    int zero_count = 0;
+    
+    char direction;
+    int amount;
+    char line_buffer[100];
+
+    while (fgets(line_buffer, sizeof(line_buffer), file)) {
+        if (sscanf(line_buffer, "%c%d", &direction, &amount) != 2) {
+            fprintf(stderr, "Error parsing line: %s\n", line_buffer);
+            return 1;
+        }
+
+        if (direction == 'L') {
+            current_value = (current_value - amount) % 100;
+        } else if (direction == 'R') {
+            current_value = (current_value + amount) % 100;
+        }
+
+        if (current_value == 0) {
+            zero_count++;
+        }
+    }
+
+    fclose(file);
+    
+    printf("Final Value: %d\n", current_value);
+    printf("Total times value hit 0: %d\n", zero_count);
+
+    return 0;
+}
